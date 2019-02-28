8c454d889cb237b116b20367fc8772ad55b46c2b
adrianvanderwal <a.vanderwaljnr@gmail.com>
2019-02-28 22:44:00 +0800

    Update control.lua
    Remove debug issue

--- control.lua
+++ control.lua
@@ -5,13 +5,5 @@ script.on_event(
         iteminsert {name = "burner-mining-drill", count = 1}
         --iteminsert {name = "electric-mining-drill", count = 1}
         iteminsert {name = "offshore-pump", count = 1}
-
-        if settings.startup["mi-debug"].value then
-            iteminsert {name = "pipe-to-ground", count = 100}
-            iteminsert {name = "pipe", count = 100}
-            iteminsert {name = "boiler", count = 100}
-            iteminsert {name = "steam-engine", count = 100}
-            iteminsert {name = "coal", count = 100}
-        end
     end
 )
